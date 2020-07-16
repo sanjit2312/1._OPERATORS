8e07532fe12d2e9adb887aa8e4d0f822fa7fbab2
Sanjit Kumar Singh <62151811+sanjit2312@users.noreply.github.com>
2020-07-16 20:21:09 +0530

    Add files via upload

---
+++ AssignmentOperators.java
@@ -0,0 +1,15 @@
+package operators;
+
+public class AssignmentOperators
+{
+	public static void main(String[] args)
+	{
+	    int a,b,c;
+	    a=b=c=30;
+	    System.out.println("The value of a is: "+a);
+	    System.out.println("The value of b is: "+b);
+	    System.out.println("The value of c is: "+c);
+	    
+	}
+
+}
