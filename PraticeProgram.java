04914485dffc94a7e0f3fb79ff7c6aaf4f951290
Sanjit Kumar Singh <62151811+sanjit2312@users.noreply.github.com>
2020-07-16 20:25:24 +0530

    Add files via upload

---
+++ PraticeProgram.java
@@ -0,0 +1,28 @@
+package operators;
+
+public class PraticeProgram
+{
+
+	public static void main(String[] args) 
+	{
+		
+		int a=50;
+		a+=5;           //a=55; 
+		a-=10;          //a=45;
+		a*=2;           //a=90;
+		a/=2;           //a=45;
+		a%=3;          //a=0;
+		
+		System.out.println("Value of a is: "+a);
+		
+		
+	 /* int b=15,c=5,d=20;
+	  	int h=b*c/d;          // h=15*5/d;  h=75/20;  h=3;
+	  	
+	  	System.out.println("Value of h is: "+h);
+	  	
+	 */
+
+	}
+
+}
