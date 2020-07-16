13fd19c7ecb6a83b1775606d64207e1351cc0332
Sanjit Kumar Singh <62151811+sanjit2312@users.noreply.github.com>
2020-07-16 20:20:28 +0530

    Add files via upload

---
+++ ArithmeticOperator.java
@@ -0,0 +1,25 @@
+package operators;
+
+public class ArithmeticOperator
+{
+
+	public static void main(String[] args) 
+	{
+	
+        int a=10,b=3;
+        int resultAddition=a+b;
+        int resultSubtraction=a-b;
+        int resultDivision=a/b;
+        int resultMultiplication=a*b;
+        int resultModDivision=a%b;
+        
+        
+        System.out.println("Addition Operator: "+resultAddition);
+        System.out.println("Substraction Operator: "+resultSubtraction);
+        System.out.println("Divisionion Operator: "+resultDivision);
+        System.out.println("Multiplication Operator: "+resultMultiplication);
+        System.out.println("Modulus Division Operator: "+resultModDivision);
+        
+	}
+
+}
